efeadc0a29f189afc345b1c2832e25255eb6b686
AntoineMath <antoine.mathu@gmail.com>
2024-10-13 22:04:13 +0200

    day7 optimized

--- day7.py
+++ day7.py
@@ -14,14 +14,24 @@ def part2(line: str) -> int:
   for n in line.strip().split(","):
     d[int(n)] = d.get(int(n), 0) + 1
   min_cost = float("inf") 
+  best_pos = 0
+
+  def triangular_cost(n: int) -> int:
+    return n * (n+1) / 2
 
   for pos in range(0, max(d)+1):
     pos_cost = 0
     for curr_pos, v in d.items():
-      pos_cost += functools.reduce(lambda a, b: a+b, range(1, abs(curr_pos-pos)+1), 0) * v
+      pos_cost += triangular_cost(abs(curr_pos - pos)) * v # used to approximate
     if pos_cost < min_cost:
       min_cost = pos_cost
-  return min_cost 
+      best_pos = pos
+
+  final_res = 0
+  for curr_pos, v in d.items():
+    final_res += functools.reduce(lambda a, b: a + b, range(abs(curr_pos - best_pos) + 1), 0) * v  # real computation
+  return final_res 
+
        
 
 
