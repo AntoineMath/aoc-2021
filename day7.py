aef4c316e901567027aac4fc203b670f94b31958
AntoineMath <antoine.mathu@gmail.com>
2024-10-13 21:57:13 +0200

    day7 part 2

--- day7.py
+++ day7.py
@@ -1,3 +1,5 @@
+import math
+import functools
 
 def part1(line: list[str]) -> int:
   nbs = [int(i) for i in line.strip().split(",")]
@@ -7,14 +9,27 @@ def part1(line: list[str]) -> int:
   return sum(abs(nb-median) for nb in nbs) 
 
 
-def part2(lines: list[str]) -> int:
-  pass
+def part2(line: str) -> int:
+  d = {}
+  for n in line.strip().split(","):
+    d[int(n)] = d.get(int(n), 0) + 1
+  min_cost = float("inf") 
+
+  for pos in range(0, max(d)+1):
+    pos_cost = 0
+    for curr_pos, v in d.items():
+      pos_cost += functools.reduce(lambda a, b: a+b, range(1, abs(curr_pos-pos)+1), 0) * v
+    if pos_cost < min_cost:
+      min_cost = pos_cost
+  return min_cost 
+       
 
 
 def main():
   with open("day7.data", "r") as f:
     lines = f.readlines()
   print(part1(lines[0]))
+  print(part2(lines[0]))
   
 
 
